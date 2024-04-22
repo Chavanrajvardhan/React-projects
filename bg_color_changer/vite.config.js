24861af62e9b21b3721e9e37a8a4a9bcde2b5ce7
Raj <112115798+Chavanrajvardhan@users.noreply.github.com>
2024-04-22 20:32:58 +0530

    change package.json

--- bg_color_changer/vite.config.js
+++ bg_color_changer/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/React-projects/bg_color_changer",
+  base:"/React-projects/",
   plugins: [react()],
 })
